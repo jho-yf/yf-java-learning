ada3ad5e81c28bc74f47d5727ae04437b53011d5
xujihong <xu-jihong@qq.com>
2021-12-29 00:16:42 +0800

    feat(用户线程和守护线程示例代码):

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # yf-java-learning
 Java学习Demo代码
+
+## JUC并发编程
+
+- [示例代码](https://github.com/jho-yf/yf-java-learning/yf-juc-learning)
+- [JUC学习笔记](https://github.com/jho-yf/yf-java-learning/yf-juc-learning)
---
+++ yf-juc-learning/src/main/java/cn/jho/juc/juc01/DaemonThreadDemo.java
@@ -0,0 +1,28 @@
+package cn.jho.juc.juc01;
+
+/**
+ * 若存在任何一个用户线程未结束，JVM存活。
+ * 若只剩下守护线程未结束，JVM结束。
+ *
+ * @author JHO xu-jihong@qq.com
+ * @date 2021-12-28 23:54
+ */
+public class DaemonThreadDemo {
+
+    public static void main(String[] args) {
+        Thread myThread = new Thread(() -> {
+            // Thread.currentThread().isDaemon() 判断当前线程是否是守护线程。true：守护线程 false：用户线程
+            System.out.println(Thread.currentThread().getName() + "::" + Thread.currentThread().isDaemon());
+            while (true) {
+
+            }
+        }, "myThread");
+
+        // 设置守护线程
+        // myThread.setDaemon(true);
+
+        myThread.start();
+        System.out.println(Thread.currentThread().getName() + "结束...");
+    }
+
+}
