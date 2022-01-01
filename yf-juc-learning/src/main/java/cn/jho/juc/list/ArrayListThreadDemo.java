9a4bd583e3b29bbdd42cb53379d7e229563dc61b
xujihong <xu-jihong@qq.com>
2022-01-01 14:52:16 +0800

    feat: List的线程不安全演示以及解决方案

---
+++ yf-juc-learning/src/main/java/cn/jho/juc/list/ArrayListThreadDemo.java
@@ -0,0 +1,26 @@
+package cn.jho.juc.list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * list线程不安全
+ *
+ * @author JHO xu-jihong@qq.com
+ * @date 2022-01-01 13:42
+ */
+public class ArrayListThreadDemo {
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+}
---
+++ yf-juc-learning/src/main/java/cn/jho/juc/list/CopyOnWriteArrayListThreadDemo.java
@@ -0,0 +1,23 @@
+package cn.jho.juc.list;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author JHO xu-jihong@qq.com
+ * @date 2022-01-01 14:24
+ */
+public class CopyOnWriteArrayListThreadDemo {
+
+    public static void main(String[] args) {
+        List<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+}
