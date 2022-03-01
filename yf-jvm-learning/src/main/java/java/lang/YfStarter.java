dadc5128b536f5d2c1b5e0c80c94a0a2521671ac
jho-yf <xu-jihong@qq.com>
2022-03-02 00:26:27 +0800

    feat: 沙箱保护机制演示

---
+++ yf-jvm-learning/src/main/java/java/lang/YfStarter.java
@@ -0,0 +1,13 @@
+package java.lang;
+
+/**
+ * @author JHO xu-jihong@qq.com
+ * @date 2022-03-02 0:16
+ */
+public class YfStarter {
+
+    public static void main(String[] args) {
+        System.out.println("hello!yf starter");
+    }
+
+}
